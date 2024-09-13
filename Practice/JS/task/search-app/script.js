b09ff78c78f5a6a0c964e158e03006c4e0413a4a
Bhavesh2024 <bparmar1722004@gmail.com>
2024-09-13 07:37:22 +0530

    pending functionality of search app

--- Practice/JS/task/search-app/script.js
+++ Practice/JS/task/search-app/script.js
@@ -1,17 +1,19 @@
-const validateData = (event) =>{
-  const {name,value} = event.target;
-  // console.log(name);
-  // console.log(value);
-  console.log(isValidData(name,value));
-}
+const validateData = (event) => {
+	const { name, value } = event.target;
+	// console.log(name);
+	// console.log(value);
+	console.log(name);
+	console.log(isValidData(name, value));
+};
 
-const isValidData = (name,value) =>{
-  const namePattern = /([a-zA-Z]+)/g;
-  const emailPattern = '';
-  const phonePattern = '';
-  const passwordPattern = '';
-  console.log()
-  switch(name){
-    case 'fname' || 'lname' : return namePattern.test(value); 
-  }
-}
+const isValidData = (name, value) => {
+	const namePattern = /^[a-zA-Z]+$/;
+	const emailPattern = "";
+	const phonePattern = "";
+	const passwordPattern = "";
+	console.log();
+	switch (name) {
+		case "fname" || "lname":
+			return namePattern.test(value);
+	}
+};
